0a1e71c47102a494384cfb6ddc4ca20157a4303b
donghua <997726077@qq.com>
2023-08-07 16:13:48 +0800

    nvim

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = { width = 30 },
+      filesystem = {
+        filtered_items = { hide_dotfiles = false },
+      },
+    },
+  },
+}
